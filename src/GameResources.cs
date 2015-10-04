682ce7b6e7d613f0786f7473be2fab5371220aae
simonjdaniel <simonjdaniel@gmail.com>
2015-10-04 18:34:33 +1100

    Updated Sound Files for Win and Lose

--- src/GameResources.cs
+++ src/GameResources.cs
@@ -52,8 +52,8 @@ public static class GameResources
 		NewSound("Sink", "sink.wav");
 		NewSound("Siren", "siren.wav");
 		NewSound("Miss", "torpedoLaunch.wav");
-		NewSound("Winner", "winning_sound.wav");
-		NewSound("Lose", "loser_sound.wav");
+		NewSound("Winner", "win_sound.wav");
+		NewSound("Lose", "lose_sound.wav");
 	}
 
 	private static void LoadMusic()
